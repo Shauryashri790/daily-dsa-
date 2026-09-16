ee9143eda2f3515aa9df6bbc6c63313e3193ddc9
Shaurya Shrivastava <shauryashri9@gmail.com>
2026-09-16 22:46:14 +0530

    Time: 25 ms (45.77%), Space: 48.9 MB (34.44%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+       int result = 0;
+       int current = 0;
+       Map<Integer,Integer> map = new HashMap();
+       map.put(0,1);
+       for(int i : nums){
+        current += i;
+        result += map.getOrDefault (current - k,0);
+        map.put(current,map.getOrDefault (current,0)+1);
+       }
+
+
+
+       return result; 
+    }
+}
